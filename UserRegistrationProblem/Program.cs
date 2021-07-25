0406808e6c3efdb058d74b9a9020bd2e1e51954e
Milind_77 <milinddhote77@gmail.com>
2021-07-26 00:49:38 +0530

    [Milind] Add:- Validating First Name.

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -4,10 +4,17 @@ namespace UserRegistrationProblem
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("<--------Welcome to User Registration Problem--------->");
             Console.WriteLine("-------------------------------------------------------");
+
+            Console.Write("Enter First Name :- ");
+            string firstName = Console.ReadLine();
+
+            // Creating object
+            RegexTest R = new RegexTest();
+            Console.WriteLine(R.TestFirstName(firstName));
         }
     }
 }
---
+++ UserRegistrationProblem/RegexTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistrationProblem
+{
+    class RegexTest
+    {
+        public string firstName = "^[A-Z]{1}[a-zA-z]{2,}";
+
+        /// <summary>
+        /// Method to test First Name
+        /// </summary>
+        /// <param name="fName"></param>
+        /// <returns></returns>
+        public bool TestFirstName(string fName)
+        {
+            Console.Write("First Name is Valid (True/False) :- ");
+            return Regex.IsMatch(fName, firstName);
+        }
+    }
+}
